cc4784ce03e0a4eaaf341bca419715803fe83bd2
sanketkatrajkar <135363208+sanketkatrajkar@users.noreply.github.com>
2025-09-11 12:09:31 +0530

    26 completed

--- Assignment_26/program4.c
+++ Assignment_26/program4.c
@@ -1,31 +1,32 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // File Name   :  program1.java
 // Author      :  Sanket Ashok Katrajkar
-// Date        :  10/09/2025
+// Date        :  11/09/2025
 //////////////////////////////////////////////////////////////////////////////////////////////////
 /*
  *  Description : Q1 : Accept the string from the user and accept the one character form user 
-                       and return the fist occarance of character
+                       and return the last occarance of character
  */
 
 
 #include<stdio.h>
 #include<stdbool.h>
 
-int firstOcc(char *str, char ch)
+int LastOcc(char *str, char ch)
 {
     int i= 0;
+    int index = 0;
 
-    while(str[i] != '\0')
+    while(str[i] != '\0') // sanket katrajkar k 
     {
         if(str[i] == ch)
         {
-            return i;
+            index = i;
         }
         i++;
     }
 
-    return - 1;
+    return index;
 }
 
 
@@ -41,9 +42,9 @@ int main()
     printf("Enter character that you want to search : \n");
     scanf(" %c",&ch);
 
-    iRet = firstOcc(arr, ch);
+    iRet = LastOcc(arr, ch);
 
-    printf("FistOccurance is %d ", iRet);
+    printf("LastOcc Occurance is %d ", iRet);
 
     return 0;
 }
