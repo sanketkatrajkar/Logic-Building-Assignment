8f8d5985f0104f1a4c143e72921bd6a61f9c139b
sanketkatrajkar <135363208+sanketkatrajkar@users.noreply.github.com>
2025-11-24 23:57:44 +0530

    Assignment 54 done

--- Assignment_54/program2.java
+++ Assignment_54/program2.java
@@ -1,16 +1,56 @@
 package Assignment_54;
 
+import java.util.Scanner;
+
 class ArrayX
 {
+    public void ReplaceSumDigit(int Arr[])
+    {
+        for(int i = 0; i < Arr.length; i++)
+        {
+            int num = Arr[i];
+            int digit = 0, sum = 0;
 
+            while(num != 0)
+            {
+                digit = num % 10;
+                sum = sum + digit;
+                num = num / 10;
+            }
+
+            Arr[i] = sum;
+        }
+    }
 }
+
+
 public class program2 
 {
     public static void main(String A[])
     {
         ArrayX aobj = new ArrayX();
-        
 
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the size of Array : ");
+        int size = sobj.nextInt();
+
+        int Arr[] = new int[size];
+
+        System.out.println("Enter the element of array :");
+        for(int i = 0; i < Arr.length; i++)
+        {
+            Arr[i] = sobj.nextInt();
+        }
+        
+        aobj.ReplaceSumDigit(Arr);
+
+        System.out.println("Output after Sumation ");
+
+        for(int i = 0; i < Arr.length; i++)
+        {
+            System.out.print(Arr[i] + " ");
+        }
     }
     
 }
--- Assignment_54/program4.java
+++ Assignment_54/program4.java
@@ -1,16 +1,45 @@
 package Assignment_54;
 
+import java.util.*;
+
 class ArrayX
 {
+    public int CountCapital(char Arr[])
+    {
+        int iCount = 0;
 
+        // Count capital letters
+        for(int i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i] >= 'A' && Arr[i] <= 'Z')
+            {
+                iCount++;
+            }
+        }
+
+        return iCount;
+    }
 }
+
 public class program4
 {
     public static void main(String A[])
     {
         ArrayX aobj = new ArrayX();
-        
 
+        Scanner sobj = new Scanner(System.in);
+        char Arr[] = new char[5];
+
+        System.out.println("Enter characters (50 characters):");
+
+        // Correct loop for reading characters
+        for(int i = 0; i < Arr.length; i++)
+        {
+            Arr[i] = sobj.next().charAt(0);  
+        }
+
+        int iRet = aobj.CountCapital(Arr);
+
+        System.out.println("Number of capital characters: " + iRet);
     }
-    
 }
